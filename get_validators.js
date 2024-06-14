392e7793cf9c6d10c1d756ceb5a52cacdd92f5db
Nejc Schneider <nejc.sch@gmail.com>
2024-06-14 15:26:15 +0200

    print data

--- get_validators.js
+++ get_validators.js
@@ -4,6 +4,12 @@ const main = async() => {
     const connection = new Connection(
         clusterApiUrl("devnet"), "processed");
     const { current, delinquent } = await connection.getVoteAccounts();
+    console.log('all validators: ' + current.concat(delinquent).length);
+    console.log('current validators: ' + current.length);
+    console.log(current[0]); //prints an example of the validator.
+};
+
+
 const runMain = async() => {
     try {
         await main();
