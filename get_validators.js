004043992965490936e378b827bf22f4054d9f93
Nejc Schneider <nejc.sch@gmail.com>
2024-06-14 15:25:57 +0200

    init constants

--- get_validators.js
+++ get_validators.js
@@ -1,6 +1,9 @@
 const { Connection, clusterApiUrl } = require("@solana/web3.js");
 
 const main = async() => {
+    const connection = new Connection(
+        clusterApiUrl("devnet"), "processed");
+    const { current, delinquent } = await connection.getVoteAccounts();
 const runMain = async() => {
     try {
         await main();
