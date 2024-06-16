b4688bd6331c7f28ce38c10e97f704d143c9c86d
Nejc Schneider <nejc.sch@gmail.com>
2024-06-16 14:36:13 +0200

    clean the main function

--- get_delegators_by_validator.js
+++ get_delegators_by_validator.js
@@ -3,49 +3,7 @@ const { Connection, clusterApiUrl, Keypair, LAMPORTS_PER_SOL, StakeProgram, Auth
 const main = async () => {
     const connection = new Connection(
         clusterApiUrl("devnet"), "processed");
-    const wallet = Keypair.generate();
-    const airdropSignature = await connection.requestAirdrop(
-        wallet.publicKey, 1 * LAMPORTS_PER_SOL);
-    await connection.confirmTransaction(airdropSignature);
-
-    // Verify balance
-    /*     const balance = await connection.getBalance(wallet.publicKey);
-        console.log(balance);
-     */
-    // Create stake account
-    const stakeAccount = Keypair.generate();
-    const minimumRent = await connection.getMinimumBalanceForRentExemption(
-        StakeProgram.space);
-    const amountToStake = minimumRent * (0.5 * LAMPORTS_PER_SOL);
-    const createStakeAccountTx = StakeProgram.createAccount({
-        authorized: new Authorized(wallet.publicKey, wallet.publicKey),
-        fromPubkey: wallet.publicKey,
-        lamports: amountToStake,
-        lockup: new Lockup(0, 0, wallet.publicKey), // 0 = already expired.
-        stakePubkey: stakeAccount.publicKey
     });
-    const createStakeAccountTxId = await sendAndConfirmTransaction(
-        connection, createStakeAccountTx, [wallet, stakeAccount]
-    ); // Should return tx id.
-
-    console.log(`Stake account created. Tx Id: ${createStakeAccountTxId}`);
-    let stakeBalance = await connection.getBalance(stakeAccount.publicKey);
-    console.log(`Stake account balance: ${stakeBalance / LAMPORTS_PER_SOL} SOL`);
-    let stakeStatus = await connection.getStakeActivation(stakeAccount.publicKey);
-    console.log(`Stake account status: ${stakeStatus.state}`);
-
-    // Validators delegation
-    const validators = await connection.getVoteAccounts();
-    const selectedValidator = validators.current[0];
-    const selectedValidatorPubkey = new PublicKey(selectedValidator.votePubkey);
-    const delegateTx = StakeProgram.delegate({
-        stakePubkey: stakeAccount.publicKey,
-        authorizedPubkey: wallet.publicKey,
-        votePubkey: selectedValidatorPubkey,
-    });
-
-    const delegateTxId = await sendAndConfirmTransaction(connection, delegateTx, [wallet]);
-    console.log(`Stake account delegated to ${selectedValidatorPubkey}, Tx Id: ${delegateTxId}`);
 
     stakeStatus = await connection.getStakeActivation(stakeAccount.publicKey);
     console.log(`Stake account status: ${stakeStatus.state}`);
